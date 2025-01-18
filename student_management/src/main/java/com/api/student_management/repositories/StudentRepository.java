828df56e2b43d2d52ba5126b07c71a027a1811c1
itsmraga-hub <itsragamit@gmail.com>
2025-01-18 11:12:46 +0300

    Add method to get active students & inactive students

--- student_management/src/main/java/com/api/student_management/repositories/StudentRepository.java
+++ student_management/src/main/java/com/api/student_management/repositories/StudentRepository.java
@@ -3,7 +3,12 @@ package com.api.student_management.repositories;
 import com.api.student_management.entities.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface StudentRepository extends JpaRepository<Student, Long> {
     // Additional custom methods for student management
+
+    List<Student> findByStatus(Integer status);
+
 
 }
