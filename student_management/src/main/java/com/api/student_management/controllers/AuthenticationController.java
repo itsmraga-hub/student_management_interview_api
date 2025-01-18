4209c4088616f53d3aa3d4424673aa0c50c704d7
itsmraga-hub <itsragamit@gmail.com>
2025-01-18 11:08:10 +0300

    Modify Authentication service to allow login

--- student_management/src/main/java/com/api/student_management/controllers/AuthenticationController.java
+++ student_management/src/main/java/com/api/student_management/controllers/AuthenticationController.java
@@ -7,6 +7,8 @@ import com.api.student_management.dtos.RegisterUserDto;
 import com.api.student_management.entities.User;
 import com.api.student_management.services.AuthenticationService;
 import com.api.student_management.services.JwtService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,6 +23,8 @@ public class AuthenticationController {
     private final JwtService jwtService;
 
     private final AuthenticationService authenticationService;
+    private static final Logger logger = (Logger) LoggerFactory.getLogger(AuthenticationController.class);
+
 
     public AuthenticationController(JwtService jwtService, AuthenticationService authenticationService) {
         this.jwtService = jwtService;
@@ -36,10 +40,13 @@ public class AuthenticationController {
 
     @PostMapping("/login")
     public ResponseEntity<LoginResponse> authenticate(@RequestBody LoginUserDto loginUserDto) {
+        logger.info("User trying to login: {}", loginUserDto);
         User authenticatedUser = authenticationService.authenticate(loginUserDto);
-
+        logger.info("Authenticated User to login: {}", authenticatedUser);
         String jwtToken = jwtService.generateToken(authenticatedUser);
-
+        logger.info("User trying to login: {}", loginUserDto);
+        logger.info("Generated JWT Token: {}", jwtToken);
+        logger.info("User trying to login: {}", authenticatedUser);
         LoginResponse loginResponse = new LoginResponse().setToken(jwtToken).setExpiresIn(jwtService.getExpirationTime());
 
         return ResponseEntity.ok(loginResponse);
