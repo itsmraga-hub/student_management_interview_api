d9f9345f8fdafdd31f3e55b78a8453d5b5783af9
itsmraga-hub <itsragamit@gmail.com>
2025-02-17 15:51:56 +0300

    Add AppRunners to practice Async & CompletableFuture

---
+++ student_management/src/main/java/com/api/student_management/initializer/AppRunner.java
@@ -0,0 +1,46 @@
+package com.api.student_management.initializer;
+
+import com.api.student_management.entities.GitHubUser;
+import com.api.student_management.services.GitHubLookupService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.CompletableFuture;
+
+@Component
+public class AppRunner implements CommandLineRunner {
+
+    private static final Logger logger = LoggerFactory.getLogger(AppRunner.class);
+
+    private final GitHubLookupService gitHubLookupService;
+
+    public AppRunner(GitHubLookupService gitHubLookupService) {
+        this.gitHubLookupService = gitHubLookupService;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        // Start the clock
+        long start = System.currentTimeMillis();
+
+        // Kick of multiple, asynchronous lookups
+        CompletableFuture<GitHubUser> page1 = gitHubLookupService.findUser("PivotalSoftware");
+        CompletableFuture<GitHubUser> page2 = gitHubLookupService.findUser("CloudFoundry");
+        CompletableFuture<GitHubUser> page3 = gitHubLookupService.findUser("Spring-Projects");
+        CompletableFuture<GitHubUser> page4 = gitHubLookupService.findUser("itsmraga-hub");
+
+        // Wait until they are all done
+        CompletableFuture.allOf(page1,page2,page3).join();
+
+        // Print results, including elapsed time
+        logger.info("Elapsed time: " + (System.currentTimeMillis() - start));
+        logger.info("--> " + page1.get());
+        logger.info("--> " + page2.get());
+        logger.info("--> " + page3.get());
+        logger.info("--> " + page4.get());
+
+    }
+
+}
